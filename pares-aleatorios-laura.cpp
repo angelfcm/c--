2e5487be391a05492e0677282a4f4145aed24bf7
Angel Carriola <angelfcm1@gmail.com>
2013-09-08 00:22:04 -0500

    código arreglado por Angel

--- pares-aleatorios-laura.cpp
+++ pares-aleatorios-laura.cpp
@@ -3,19 +3,21 @@
 #include <cstdlib>
 
 int main (){
-int lista [10];
+    int lista [10];
+    int z = 0;
+    srand (time(NULL));
+    printf("10 numeros pares: \n");
 
-srand (time(NULL));
-printf("10 numeros pares: \n");
-for (int i=0; i<10; i++){
-lista [i]= rand ()%199;
-if (lista [i]%2==0){
-int z = z + lista [i];
-printf ("numeros: %i\n",lista [i]);
-}else {
-    i--;
-}
-}
-printf("%i la suma es: ", z);
-return 0;
+    for (int i=0; i<10; i++){
+        lista [i]= rand ()%199;
+        if (lista [i]%2!=0){
+            lista [i] += 1;
+        }
+        z += lista [i];
+        printf ("numeros: %i\n",lista [i]);
+    }
+
+    printf("la suma es: %i", z);
+
+    return 0;
 }
