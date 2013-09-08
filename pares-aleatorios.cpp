9608a3f7b83a74b9bea7c07129e50d78e64a05aa
Angel Carriola <angelfcm1@gmail.com>
2013-09-07 23:47:40 -0500

    sumando los pares e imprimiendo el resultado

--- pares-aleatorios.cpp
+++ pares-aleatorios.cpp
@@ -6,14 +6,17 @@ int main ()
 {
     int a;
     int x;
+    int suma = 0;
     srand(time(NULL));
     for (int x=0; x<10; x++) {
         a = rand() % 115 + 1;
+        suma += a;
         if(a%2!=0){
             a=a+1;
         }
         printf("%d\n",a);
     }
+    printf("SUMA = %i", suma);
 
     return 0;
 
